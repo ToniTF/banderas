8dc730a863dd2d2aacc4e88a237ca3fceaf527e9
Tonitf <subcero00@hotmail.com>
2025-02-10 19:47:42 +0100

    spin

--- js/banderas.js
+++ js/banderas.js
@@ -1,12 +1,14 @@
 fetch("https://restcountries.com/v3.1/all")
-.then(response => response.json())
-.then(data => {
-    let banderas = document.getElementById("banderas");
-    for (let i = 0; i < data.length; i++) {
-        let img = document.createElement("img");
-        img.src = data[i].flags.png;
-        img.alt = data[i].name.common;
-        banderas.appendChild(img);
-    }
-})
-.catch(error => console.error('Error:', error));
+    .then(response =>{
+        $("#cargando").hide();
+        return response.json()})
+    .then(data => {
+        let banderas = document.getElementById("banderas");
+        for (let i = 0; i < data.length; i++) {
+            let img = document.createElement("img");
+            img.src = data[i].flags.png;
+            img.alt = data[i].name.common;
+            banderas.appendChild(img);
+        }
+    })
+    .catch(error => console.error(error));
