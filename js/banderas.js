105e262143790c15dc3b98bfa06b38238a6463c0
Tonitf <subcero00@hotmail.com>
2025-02-11 18:32:28 +0100

    actual

--- js/banderas.js
+++ js/banderas.js
@@ -1,4 +1,8 @@
-fetch("https://restcountries.com/v3.1/all")
+function onMouseOver(params) {
+    console.log("Mouse sobre la bandera de: " + params.target.alt);
+}
+function cargarBanderas(){
+    fetch("https://restcountries.com/v3.1/all")
     .then(response =>{
         $("#cargando").hide();
         return response.json()})
@@ -7,8 +11,16 @@ fetch("https://restcountries.com/v3.1/all")
         for (let i = 0; i < data.length; i++) {
             let img = document.createElement("img");
             img.src = data[i].flags.png;
+            img.className = "bandera";
             img.alt = data[i].name.common;
+            img.addEventListener("mouseover", onMouseOver);
             banderas.appendChild(img);
         }
+        $(".bandera").click(function(){ 
+            alert("El país seleccionado es: " + $(this).attr("alt"));
+        })
     })
-    .catch(error => console.error(error));
+    .catch(error => console.error(error))
+};
+
+setTimeout(cargarBanderas, 5000);
